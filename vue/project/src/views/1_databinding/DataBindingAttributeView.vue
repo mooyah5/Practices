13aed660acc8722e9486d7d47bf1a2a4df9cdaaf
mooyah5 <qorgkssk123@gmail.com>
2022-07-15 01:29:05 +0900

    0715 vue3_01_DataBinding

---
+++ vue/project/src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-model="userId" readonly>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ vue/project/src/views/1_databinding/DataBindingCheckboxView.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <div>
+    <input type="checkbox" name="" id="html" value="HTML" v-model="favoriteLang">
+    <label for="">HTML</label>
+  </div>
+  <div>
+    <input type="checkbox" name="" id="css" value="CSS" v-model="favoriteLang">
+    <label for="">CSS</label>
+  </div>
+  <div>
+    <input type="checkbox" name="" id="js" value="JS" v-model="favoriteLang">
+    <label for="">JS</label>
+  </div>
+  <div>선택한 지역: {{ favoriteLang }}</div>
+</div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      favoriteLang: []
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>

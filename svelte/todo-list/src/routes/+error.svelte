0ff3b2dbb287fd95870b82dc3a08c9189da97893
mooyah5 <qorgkssk123@gmail.com>
2023-08-22 00:32:35 +0900

    [svelte] ADD todoList (add)

--- svelte/todo-list/src/routes/+error.svelte
+++ svelte/todo-list/src/routes/+error.svelte
@@ -1,7 +1,6 @@
-<script>
+<!-- <script>
 	import { error } from '@sveltejs/kit';
 </script>
 
 <h1>error!</h1>
-<h3>%sveltekit.status% {error.message}</h3>
-<!-- <h3>{}</h3> -->
+<h3>{$error.message}</h3> -->
--- svelte/todo-list/src/routes/+page.svelte
+++ svelte/todo-list/src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to Todo List!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let inputText = '';
+
+	let todoList = [
+		{
+			id: 1,
+			completed: false,
+			content: '스벨트 학습하기'
+		},
+		{
+			id: 2,
+			completed: true,
+			content: '타입스크립트 학습하기'
+		}
+	];
+
+	const addTodo = () => {
+		todoList = [...todoList, { id: todoList.length + 1, content: inputText, completed: false }];
+	};
+
+	const changeCompleted = () => {
+		!todo.completed ? true : false;
+	};
+</script>
+
+<div class="container">
+	<h1>To Do List</h1>
+	<input type="text" bind:value={inputText} placeholder="할 일을 입력해 주세요!" />
+	<button on:click={addTodo}>저장</button>
+
+	<ul>
+		{#each todoList as todo}
+			<li>
+				<input type="checkbox" on:click={changeCompleted} checked={todo.completed} />
+				{todo.id}. {todo.content}
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	@font-face {
+		font-family: 'HakgyoansimWoojuR';
+		src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-2@1.0/HakgyoansimWoojuR.woff2')
+			format('woff2');
+		font-weight: normal;
+		font-style: normal;
+	}
+	.container {
+		font-family: 'HakgyoansimWoojuR';
+	}
+	ul {
+		padding-left: 0;
+	}
+	li {
+		list-style: none;
+		padding-top: 4px;
+	}
+</style>

7cdb4d0feab62444fbc07f98ef4dc026a946e472
mooyah5 <qorgkssk123@gmail.com>
2024-01-09 18:32:26 +0900

    feat: threejs test

---
+++ threejs/gsap-test1/app/page.tsx
@@ -0,0 +1,9 @@
+import MainDOM from "@/components/home/MainDom";
+
+export default function Home() {
+  return (
+    <main className="flex">
+      <MainDOM></MainDOM>
+    </main>
+  );
+}
---
+++ threejs/test-three-1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+function App() {
+  return (
+    <div className="App">
+      <canvas></canvas>
+    </div>
+  );
+}
+
+export default App;

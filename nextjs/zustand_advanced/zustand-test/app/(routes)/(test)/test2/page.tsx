5cfc3a5716d404da23b9dc5f26be4db90561f58f
mooyah5 <qorgkssk123@gmail.com>
2024-02-22 20:30:57 +0900

    feat: zustand

--- nextjs/zustand_advanced/zustand-test/app/(routes)/(test)/test2/page.tsx
+++ nextjs/zustand_advanced/zustand-test/app/(routes)/(test)/test2/page.tsx
@@ -1,10 +1,15 @@
+"use client";
 import styles from "./test2.module.scss";
+import useTestAuthStore from "@/app/lib/testAuth";
 
 export default function Test2() {
+  const { name, age } = useTestAuthStore();
   return (
     <>
       <div className={styles.header}>SubHeader</div>
       <div>Test2</div>
+      <div>{name}</div>
+      <div>{age}</div>
     </>
   );
 }

425c07024ed053b5281b5be6b1d898776daca773
mooyah5 <qorgkssk123@gmail.com>
2024-02-22 16:26:05 +0900

    test: add test routes channel and apply scss modules

---
+++ nextjs/zustand_advanced/zustand-test/app/(test)/test2/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./test2.module.css";
+
+export default function Test2() {
+  return (
+    <>
+      <div className={styles.header}>SubHeader</div>
+      <div>Test2</div>
+    </>
+  );
+}

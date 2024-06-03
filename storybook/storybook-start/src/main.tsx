767ee5ce834160fbc308b96ba4eae23be850af6c
mooyah5 <qorgkssk123@gmail.com>
2024-06-03 22:57:51 +0900

    feat: jason-kang-UI

--- storybook/storybook-start/src/main.tsx
+++ storybook/storybook-start/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'jason-kang-ui/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

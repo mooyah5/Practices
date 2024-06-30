28c9e091bd614f42622a42ceb3241ee8f838de77
mooyah5 <qorgkssk123@gmail.com>
2024-07-01 01:33:22 +0900

    feat: Button 컴포넌트를 통한 폴더구조 설명 및  Label 컴포넌트 작성
    Button story만 story에 저장하고, components 폴더에 Button.tsx , css 를 저장한다.
    - 이유: story가 빌드될 필요가 없으므로 분리한다.
    
    Buttons.tsx 파일 속 주석보다는 Button.stories.ts 파일 속 description 사용 권장
    
    preview.ts에 css 등 import

--- storybook/storybook-start/.storybook/preview.ts
+++ storybook/storybook-start/.storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import '../src/index.css'
+// 추후 font설정도 추가
 
 const preview: Preview = {
   parameters: {
--- storybook/storybook-start/src/stories/Button.stories.ts
+++ storybook/storybook-start/src/stories/Button.stories.ts
@@ -1,29 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Button } from './Button';
+import { Button } from '../components/Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// 버튼이 받는 prop들 정리
 const meta = {
-  title: 'Example/Button',
-  component: Button,
+  title: 'EXAMPLE/Button',  // 폴더 위치
+  component: Button,        // 
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: 'color', description:"버튼의 배경 컬러" },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     primary: true,

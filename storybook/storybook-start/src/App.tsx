e212443547251e4809f7bf7bad2c381a0ab1a78f
mooyah5 <qorgkssk123@gmail.com>
2024-07-01 00:51:53 +0900

    refac: add tailwind theme (fontSize, colors)

--- storybook/storybook-start/src/App.tsx
+++ storybook/storybook-start/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1 className="underline">Vite + React</h1>
+      <h1 className="underline text-secondary text-2xl">Vite + React</h1>
       <div className="card">
         <PrimaryButton onClick={() => setCount((count) => count + 1)}>
           count is {count}

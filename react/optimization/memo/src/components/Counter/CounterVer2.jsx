eb59c192c72d67e6d77cef9f1072ea9711397a8f
mooyah5 <qorgkssk123@gmail.com>
2024-07-22 21:03:08 +0900

    [React] OPT - Counter 컴포넌트 ver2 제작 (historic)

---
+++ react/optimization/memo/src/components/Counter/CounterVer2.jsx
@@ -0,0 +1,69 @@
+import { memo, useCallback, useMemo, useState } from "react";
+
+import IconButton from "../UI/IconButton.jsx";
+import MinusIcon from "../UI/Icons/MinusIcon.jsx";
+import PlusIcon from "../UI/Icons/PlusIcon.jsx";
+import CounterOutput from "./CounterOutput.jsx";
+import { log } from "../../log.ts";
+
+function isPrime(number) {
+  log("Calculating if is prime number", 2, "other");
+  if (number <= 1) {
+    return false;
+  }
+
+  const limit = Math.sqrt(number);
+
+  for (let i = 2; i <= limit; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const CounterVer2 = memo(function Counter({ initialCount }) {
+  log("<Counter /> rendered", 1);
+
+  // 의존성에 사용하기 위한 initialCount 배열
+  const [counterChanges, setCounterChanges] = useState([initialCount]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange,
+    0
+  );
+
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
+  const handleDecrement = useCallback(() => {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevChanges) => [-1, ...prevChanges]);
+  }, []);
+
+  const handleIncrement = useCallback(() => {
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevChanges) => [1, ...prevChanges]);
+  }, []);
+
+  return (
+    <section className="counter">
+      <p className="counter-info">
+        The initial counter value was <strong>{initialCount}</strong>. It{" "}
+        <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
+      </p>
+      <p>
+        <IconButton icon={MinusIcon} onClick={handleDecrement}>
+          Decrement
+        </IconButton>
+        <CounterOutput value={currentCounter} />
+        <IconButton icon={PlusIcon} onClick={handleIncrement}>
+          Increment
+        </IconButton>
+      </p>
+    </section>
+  );
+});
+export default CounterVer2;

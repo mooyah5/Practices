eb165f9034212a06569df8ea2355a058e31d3792
mooyah5 <qorgkssk123@gmail.com>
2024-07-22 19:09:29 +0900

    [react optimization] React.memo

--- react/optimization/memo/src/components/Counter/Counter.jsx
+++ react/optimization/memo/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -23,7 +23,11 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+// React.memo로 컴포넌트 감싸기
+// 컴포넌트가 정상 재실행 될 때 이전 props값과 새로 받을 props값을 비교하여
+// 변경이 없다면 memo가 렌더링을 저지하고 이전에 렌더된 결과를 재사용한다. (자식들도 자연히 마찬가지)
+// (부모 컴포넌트에 의해 실행되었을 때만 유효하며, 내부적 변화는 모르쇠임)
+const Counter = memo(function Counter({ initialCount }) {
   log("<Counter /> rendered", 1);
   const initialCountIsPrime = isPrime(initialCount);
 
@@ -54,4 +58,5 @@ export default function Counter({ initialCount }) {
       </p>
     </section>
   );
-}
+});
+export default Counter;

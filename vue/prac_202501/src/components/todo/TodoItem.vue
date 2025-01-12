4963fdc6f3ab39c95b0b9c00882227e81a7de05f
mooyah5 <qorgkssk123@gmail.com>
2025-01-12 22:45:59 +0900

    init: Vue3 simple todo project (1)

---
+++ vue/prac_202501/src/components/todo/TodoItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  todo: { name: String, idx: Number, isDone: Boolean },
+})
+</script>
+
+<template>
+  <div style="display: flex; gap: 4px; padding: 2px 0">
+    <input
+      :id="todo.idx"
+      type="checkbox"
+      :checked="todo.isDone"
+      @change="$emit('update:isDone', !todo.isDone)"
+    />
+    <label :for="todo.idx" style="color: white">{{ todo.text }}</label>
+    <button @click="$emit('delete', todo.idx)">삭제</button>
+  </div>
+</template>
